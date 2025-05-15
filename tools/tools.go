622dbaafc51e08c6eb1bd9e5de104883b7e3f692
Bharath B <bhb@redhat.com>
2025-05-15 12:14:35 +0530

    NO-JIRA: vendor in packages required by the prow ci

--- tools/tools.go
+++ tools/tools.go
@@ -10,4 +10,10 @@ import (
 	// Makefile
 	_ "github.com/openshift/build-machinery-go"
 	_ "github.com/go-bindata/go-bindata/go-bindata"
+
+	// prow-ci
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
+	_ "sigs.k8s.io/kustomize/kustomize/v5"
 )
