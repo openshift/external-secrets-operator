320e38c96ccb8d039c0a243279a4dd3eb3a18fcb
siddhi bhor <sbhor@redhat.com>
2025-06-25 14:23:24 +0530

    Adds secrets for pushsecret

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,37 +1,60 @@
+/*
+Copyright 2025.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package e2e
 
 import (
 	"context"
 	"embed"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	utils "github.com/openshift/external-secrets-operator/test/utils"
+	"encoding/base64"
+	"fmt"
+	"testing"
+	"time"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"testing"
-	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/openshift/external-secrets-operator/test/utils"
 )
 
 //go:embed testdata/*
 var testassets embed.FS
 
 const (
-	operatorNamespace  = "external-secrets-operator"
-	operandNamespace   = "external-secrets"
-	secretStoreFile    = "testdata/aws_secret_store.yaml"
-	externalSecretFile = "testdata/aws_external_secret.yaml"
-	pushSecretFile     = "testdata/push_secret.yaml"
-	externalSecrets    = "testdata/external_secret.yaml"
+	operatorNamespace       = "external-secrets-operator"
+	operandNamespace        = "external-secrets"
+	secretStoreFile         = "testdata/aws_secret_store.yaml"
+	externalSecretFile      = "testdata/aws_external_secret.yaml"
+	pushSecretFile          = "testdata/push_secret.yaml"
+	externalSecrets         = "testdata/external_secret.yaml"
+	expectedSecretValueFile = "testdata/expected_value.yaml"
+	awsSecretToPushFile     = "testdata/aws_k8s_push_secret.yaml"
 )
 
-var _ = Describe("External Secrets Operator End-to-End", Ordered, func() {
+var _ = Describe("External Secrets Operator End-to-End test scenarios", Ordered, func() {
 	ctx := context.TODO()
 	var (
 		clientset     *kubernetes.Clientset
 		dynamicClient *dynamic.DynamicClient
 		loader        utils.DynamicResourceLoader
+		awsSecretName string
 	)
 
 	BeforeAll(func() {
@@ -44,20 +67,22 @@ var _ = Describe("External Secrets Operator End-to-End", Ordered, func() {
 		dynamicClient, err = dynamic.NewForConfig(cfg)
 		Expect(err).Should(BeNil())
 
+		awsSecretName = fmt.Sprintf("eso-e2e-secret-%s", utils.GetRandomString(5))
+
 		By("Waiting for external-secrets-operator controller-manager pod to be ready")
 		Expect(utils.VerifyPodsReadyByPrefix(ctx, clientset, operatorNamespace, []string{
 			"external-secrets-operator-controller-manager-",
 		})).To(Succeed())
 
-		By("Creating the ExternalSecrets Operator CR")
+		By("Creating the externalsecrets.openshift.operator.io/cluster CR")
 		loader.CreateFromFile(testassets.ReadFile, externalSecrets, operatorNamespace)
 	})
 
 	AfterAll(func() {
-		By("Deleting the ExternalSecrets Operator CR")
+		By("Deleting the externalsecrets.openshift.operator.io/cluster CR")
 		loader.DeleteFromFile(testassets.ReadFile, externalSecrets, operatorNamespace)
 
-		err := utils.DeleteAWSSecret("test/e2e", "eu-north-1")
+		err := utils.DeleteAWSSecret(awsSecretName, "ap-south-1")
 		Expect(err).NotTo(HaveOccurred(), "failed to delete AWS secret test/e2e")
 	})
 
@@ -70,7 +95,15 @@ var _ = Describe("External Secrets Operator End-to-End", Ordered, func() {
 		})).To(Succeed())
 	})
 
-	It("should create secrets from SecretStore and ExternalSecret", func() {
+	It("should create secrets mentioned in ExternalSecret using the referenced SecretStore", func() {
+		expectedSecretValue, err := utils.ReadExpectedSecretValue(expectedSecretValueFile)
+		Expect(err).To(Succeed())
+
+		By("Creating kubernetes secret to be used in PushSecret")
+		secretsAssetFunc := utils.ReplacePatternInAsset("${SECRET_VALUE}", base64.StdEncoding.EncodeToString(expectedSecretValue))
+		loader.CreateFromFile(secretsAssetFunc, awsSecretToPushFile, operandNamespace)
+		defer loader.DeleteFromFile(testassets.ReadFile, awsSecretToPushFile, operandNamespace)
+
 		By("Creating SecretStore")
 		loader.CreateFromFile(testassets.ReadFile, secretStoreFile, operandNamespace)
 		defer loader.DeleteFromFile(testassets.ReadFile, secretStoreFile, operandNamespace)
@@ -86,7 +119,8 @@ var _ = Describe("External Secrets Operator End-to-End", Ordered, func() {
 		)).To(Succeed())
 
 		By("Creating PushSecret")
-		loader.CreateFromFile(testassets.ReadFile, pushSecretFile, operandNamespace)
+		assetFunc := utils.ReplacePatternInAsset("${AWS_SECRET_KEY_NAME}", awsSecretName)
+		loader.CreateFromFile(assetFunc, pushSecretFile, operandNamespace)
 		defer loader.DeleteFromFile(testassets.ReadFile, pushSecretFile, operandNamespace)
 
 		By("Waiting for PushSecret to become Ready")
@@ -100,7 +134,7 @@ var _ = Describe("External Secrets Operator End-to-End", Ordered, func() {
 		)).To(Succeed())
 
 		By("Creating ExternalSecret")
-		loader.CreateFromFile(testassets.ReadFile, externalSecretFile, operandNamespace)
+		loader.CreateFromFile(assetFunc, externalSecretFile, operandNamespace)
 		defer loader.DeleteFromFile(testassets.ReadFile, externalSecretFile, operandNamespace)
 
 		By("Waiting for ExternalSecret to become Ready")
@@ -121,9 +155,7 @@ var _ = Describe("External Secrets Operator End-to-End", Ordered, func() {
 			val, ok := secret.Data["aws_secret_access_key"]
 			g.Expect(ok).To(BeTrue(), "aws_secret_access_key should be present in secret %s", secret.Name)
 
-			expectedValue := []byte("hqTTSYkFYgkw3OfQ9lFvQgtsReb1g1a+Po5Y/HNU")
-			g.Expect(val).To(Equal(expectedValue), "aws_secret_access_key does not match expected value")
-		}, time.Minute, 5*time.Second).Should(Succeed())
-
+			g.Expect(val).To(Equal(expectedSecretValue), "aws_secret_access_key does not match expected value")
+		}, time.Minute, 10*time.Second).Should(Succeed())
 	})
 })
