d3c1bf1111b53a9fa277606d7cb3a95974610e6a
Bharath B <bhb@redhat.com>
2026-08-19 20:55:17 +0530

    ESO-564: bump operator version to 1.3.0 and regenerate manifests and bundle
    Signed-off-by: Bharath B <bhb@redhat.com>

--- pkg/operator/assets/bindata.go
+++ pkg/operator/assets/bindata.go
@@ -159,7 +159,7 @@ metadata:
   labels:
     app.kubernetes.io/name: external-secrets-webhook
     app.kubernetes.io/instance: external-secrets
-    app.kubernetes.io/version: "v1.2.0"
+    app.kubernetes.io/version: "v1.3.0"
     app.kubernetes.io/managed-by: external-secrets-operator
     external-secrets.io/component: webhook
 spec:
@@ -209,7 +209,7 @@ metadata:
   labels:
     app.kubernetes.io/name: bitwarden-sdk-server
     app.kubernetes.io/instance: external-secrets
-    app.kubernetes.io/version: "v1.2.0"
+    app.kubernetes.io/version: "v1.3.0"
     app.kubernetes.io/managed-by: external-secrets-operator
 spec:
   podSelector:
@@ -254,7 +254,7 @@ metadata:
   labels:
     app.kubernetes.io/name: external-secrets-cert-controller
     app.kubernetes.io/instance: external-secrets
-    app.kubernetes.io/version: "v1.2.0"
+    app.kubernetes.io/version: "v1.3.0"
     app.kubernetes.io/managed-by: external-secrets-operator
 spec:
   podSelector:
@@ -300,7 +300,7 @@ metadata:
   labels:
     app.kubernetes.io/name: external-secrets
     app.kubernetes.io/instance: external-secrets
-    app.kubernetes.io/version: "v1.2.0"
+    app.kubernetes.io/version: "v1.3.0"
     app.kubernetes.io/managed-by: external-secrets-operator
 spec:
   podSelector:
@@ -344,7 +344,7 @@ metadata:
   labels:
     app.kubernetes.io/name: external-secrets
     app.kubernetes.io/instance: external-secrets
-    app.kubernetes.io/version: "v1.2.0"
+    app.kubernetes.io/version: "v1.3.0"
     app.kubernetes.io/managed-by: external-secrets-operator
   name: eso-sys-allow-to-dns
 spec:
@@ -398,7 +398,7 @@ metadata:
   labels:
     app.kubernetes.io/name: external-secrets
     app.kubernetes.io/instance: external-secrets
-    app.kubernetes.io/version: "v1.2.0"
+    app.kubernetes.io/version: "v1.3.0"
     app.kubernetes.io/managed-by: external-secrets-operator
 spec:
   podSelector: {}
